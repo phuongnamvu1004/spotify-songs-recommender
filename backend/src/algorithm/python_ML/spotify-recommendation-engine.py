fd12c26e899489babc40fdf7d170bdaf6ab83631
vphuongnam <nvu@oberlin.edu>
2025-03-27 22:31:32 -0400

    database limit set 65000

--- backend/src/algorithm/python_ML/spotify-recommendation-engine.py
+++ backend/src/algorithm/python_ML/spotify-recommendation-engine.py
@@ -43,7 +43,7 @@ def main():
         "tempo", 
         "valence", 
         "year"
-    ], "songs", 80000)
+    ], "songs", 65000)
 
     log_debug(f"✅ Retrieved {len(spotify_df)} songs from DB")
 
